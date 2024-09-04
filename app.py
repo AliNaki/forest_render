4b3446b15b7ec5694beae4e8177f723875adaf79
Ali Naki <122255546+AliNaki@users.noreply.github.com>
2024-09-04 22:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pickle
 from flask import Flask, request, jsonify, render_template
-import numpy as np
-import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
